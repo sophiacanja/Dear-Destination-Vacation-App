a6bfe7fab8ee288984df4884ab74138c92dff095
sophiacanja <sophia.canja1@gmail.com>
2024-01-03 16:05:05 -0800

    added the mui calendar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,24 @@ import Header from './pages/header';
 import logo from './logo.svg';
 import './App.css';
 import VacationTable from './pages/vacationTable';
+import Grid from '@mui/material/Grid';
+import Calendar from './components/Calendar.tsx';
 
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <VacationTable></VacationTable>
+      <div className="table-calendar-container">
+        <Grid container spacing={0} columnSpacing={{ xs: 0}}> 
+          <Grid item xs={6} >
+            <VacationTable></VacationTable>
+          </Grid>
+          <Grid item xs={6} style={{ margin: '0', marginTop: '10px' }}>
+            <Calendar></Calendar>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import Button from '../components/Button.tsx'
 
-const handleButtonClick = () => {
-    console.log("button clicked");
-  }
 
 const Header: React.FC = () => {
     return(
@@ -11,8 +7,7 @@ const Header: React.FC = () => {
             <header id="site-title"> Dear Destination, </header>
             <p>
                 This is an application helps you and your friends organize upcoming trips! <br></br>This is a one-stop shop to find out vacation details, itineraries, and attendees in order to avoid confusion and miscommunication when planning big trips!
-            </p>
-            <Button title="Plan New Vacation" variant = "contained" onClick={handleButtonClick}/>
+            </p>  
       </div>
     )
 } 
