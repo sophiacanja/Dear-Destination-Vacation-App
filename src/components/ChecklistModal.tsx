e8a71a1c4f25133b7b409f2ff29d0241a9b9971c
sophiacanja <sophia.canja1@gmail.com>
2024-01-06 00:09:14 -0800

    updated view checklist

--- src/components/ChecklistModal.tsx
+++ src/components/ChecklistModal.tsx
@@ -3,6 +3,7 @@ import Button from './Button.tsx';
 import './ChecklistModal.css'
 import {Modal, Typography, Box} from "@mui/material";
 import { json } from 'stream/consumers';
+import { isTemplateElement } from '@babel/types';
 
 // https://vf24p8yepd.execute-api.us-west-1.amazonaws.com/dev/get-checklist 
 type ChecklistModalProps = {
@@ -12,25 +13,14 @@ type ChecklistModalProps = {
     vacationLocation: string; 
 }
 
-interface ChecklistInfo {
-    ChecklistId: string,
-    Item: string,
-    VacationId: string
-}
 
 const ChecklistModal: React.FC<ChecklistModalProps> = ({open, onClose, vacationLocation, vacationId}) => {
-    const [checklistItems, setChecklistItems] = useState<ChecklistInfo>({
-        ChecklistId: "",
-        Item: "",
-        VacationId: ""
-    });
+    const [checklistArr, setChecklistArr] = useState([])
 
     var headersAPI: any = {
         "vacation_id" : vacationId
     }
-
-    // console.log("Vacation id " + vacationId)
-    // console.log(headersAPI)
+    console.log('Vacation id: ', vacationId)
     useEffect(() => {
         fetch(`https://vf24p8yepd.execute-api.us-west-1.amazonaws.com/dev/get-checklist`, {
             method: 'GET',
@@ -38,23 +28,27 @@ const ChecklistModal: React.FC<ChecklistModalProps> = ({open, onClose, vacationL
         })
         .then((response) => response.json()) 
         .then((data) => {
-            if(data[0] !== undefined){
-                setChecklistItems({Item : data[0].item, ChecklistId : data[0].ChecklistId, VacationId:data[0].VacationId})
-                console.log(checklistItems)
-                console.log(data)
+            if (data && data.length > 0 ){
+                const tempChecklistArr = data.map((item) => {
+                    return item.Item
+                    // console.log(item.Item)
+                })
+            console.log("Temp checklist", tempChecklistArr)
+            setChecklistArr(tempChecklistArr)
+            console.log("Updated Checklist Items:", checklistArr);  
             }
-            
-            else {
-                console.log("DATA RESPONSE IS UNDEF")
-                console.log(data)
-            }
-            
+                
         })
+                 
         .catch((err) => {
             console.error(err.message)
         })
 
     }, [vacationId])
+
+useEffect(() => {
+    console.log("USE EFFECT Updated Checklist Items:", checklistArr);
+    }, [checklistArr]);
 
     return(
         <Modal
@@ -66,6 +60,10 @@ const ChecklistModal: React.FC<ChecklistModalProps> = ({open, onClose, vacationL
             <div id="checklist-modal-container">
                 <Typography id="checklist-modal" variant="h4" component="h2" sx={{mt:2}}>
                 Trip Checklist: {vacationLocation} 
+                </Typography>
+                    
+                <Typography id="checklist-modal" variant="h6" component="h2">
+                    {checklistArr.map((item) => <div> {item} </div>)}
                 </Typography>
            
                 <Button title="Close" onClick={onClose} size="small" variant='contained'/>
