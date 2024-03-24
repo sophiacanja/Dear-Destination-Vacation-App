2f8c123580bf0f635724b81dc95e8fcf63933f64
sophiacanja <sophia.canja1@gmail.com>
2024-03-24 09:44:45 -0700

    updated header pages

--- src/pages/Header/header.tsx
+++ src/pages/Header/header.tsx
@@ -5,9 +5,11 @@ const Header: React.FC = () => {
     return(
         <div id="header-container">
             <header id="site-title"> Dear Destination, </header>
-            <p>
-                Welcome! This is an application helps you and your friends organize upcoming trips! <br></br>This is a one-stop shop to find out vacation details, itineraries, and attendees in order to avoid confusion and miscommunication when planning big trips!
-            </p>  
+            <div id="title-description"> 
+                <p>
+                    Welcome! This is an application helps you and your friends organize upcoming trips! <br></br>This is a one-stop shop to find out vacation details, itineraries, and attendees in order to avoid confusion and miscommunication when planning big trips!
+                </p>  
+            </div>
       </div>
     )
 } 
