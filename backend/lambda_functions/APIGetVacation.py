c192742d3abdf94f33bd7acf5148a808e33a4203
sophiacanja <sophia.canja1@gmail.com>
2024-03-24 11:39:01 -0700

    updated getVacation with custom exceptions

--- backend/lambda_functions/APIGetVacation.py
+++ backend/lambda_functions/APIGetVacation.py
@@ -3,6 +3,7 @@ import boto3
 import datetime
 import json
 from datetime import datetime
+from custom_exceptions import VacationPlannerDatabaseConnectionError, VacationPlannerAuroraDbError, VacationPlannerJsonOutputError
 
 #Database Configuration 
 # endpoint = 'vacationapp-dbcluster-instance-1.cmzd0vkepxf7.us-west-1.rds.amazonaws.com'
@@ -12,56 +13,77 @@ from datetime import datetime
 
 
 def lambda_handler(event, context):
-    client = boto3.client('secretsmanager')
-    response = client.get_secret_value(SecretId='vacationAppDb/MySQL')
-    secretDict = json.loads(response['SecretString'])
-    endpoint = secretDict['host']
-    username = secretDict['username']
-    password = secretDict['password']
-    database_name = 'vacationAppDb'
-    #Connection 
-    connection = pymysql.connect(host=endpoint, user=username, password=password, db=database_name)
     try: 
+        #establishing connection
+        connection = get_db_connection()
         cursor = connection.cursor()
         print(f"Is the database connection open? {connection.open}")
-
-    except Exception as e:
-        return {
-            'body': f"Error with cursor connection {e}"
-        }
     
+        #get data from vacation table
+        output = get_vacation_data(cursor)
+
+        #format vacation data
+        formatted_output = format_vacation_data(output)
+        
+        
+    except VacationPlannerDatabaseConnectionError as e: 
+        return_obj = e.get_err_obj() 
+
+    else: 
+        return_obj = {
+            'statusCode' : 200, 
+            'headers': {"Access-Control-Allow-Origin": "*"},
+            'body' : json.dumps(formatted_output)
+        }
+
+    finally: 
+        connection.commit()    
+        connection.close()
+
+    return return_obj
+
+
+#helper function that gets data from vacation table or throws an error
+def get_vacation_data(cursor):
     try:
         query = """SELECT * FROM Vacation"""
         cursor.execute(query)
+
         rows = cursor.fetchall()
-        if not rows:
-            return {
-                'statusCode': 404,
-                'body' :"No records found"
-            }
-        for row in rows:
-            print(row)
 
-        # Get column names from cursor.description
+            # Get column names from cursor.description
         columns = [desc[0] for desc in cursor.description]
-        # Convert the result set to a list of dictionaries
+            # Convert the result set to a list of dictionaries
         output = [dict(zip(columns, row)) for row in rows]
+        return output
+    
+    except Exception as e:
+        raise VacationPlannerAuroraDbError(message={e})
 
-        formatted_output = formatData(output)
-        
-        connection.commit()    
-        connection.close()
-        return{
-            'statusCode' : 200, 
-            'body' : json.dumps(formatted_output)
-        }
+    
+#helper function that esatblishes the database connection or throws an error
+def get_db_connection():
+    try: 
+        client = boto3.client('secretsmanager')
+        response = client.get_secret_value(SecretId='vacationAppDb/MySQL')
+
+
+        secretDict = json.loads(response['SecretString'])
+        print("Secret Dict:", secretDict)
+        endpoint = secretDict['host']
+        username = secretDict['username']
+        password = secretDict['password']
+        database_name = 'vacationAppDb'
+
+        #Connection 
+        connection = pymysql.connect(host=endpoint, user=username, password=password, db=database_name)
+        return connection
+    
     except Exception as e: 
-        return {
-            'statusCode' : 500, 
-            'body': f"Error querying the database {e}"
-        }
+        raise VacationPlannerDatabaseConnectionError(message={e})
         
-def formatData(output):
+        
+def format_vacation_data(output):
     try:
         for dictionary in output:
             date_obj = dictionary.get("DepartureDate")
@@ -70,10 +92,20 @@ def formatData(output):
                 dictionary["DepartureDate"] = formatted_date
         
         return output
+    
+
     except Exception as e:
-        return {
-            'body' : f"Error formatting data {e}"
-        }
+        raise VacationPlannerJsonOutputError(message={e})
+    
+
+#helper function that prints data in vacation table
+def print_vacation_table_data(cursor):
+    query = """SELECT * FROM Vacation"""
+    cursor.execute(query)
+    rows = cursor.fetchall()
+       
+    for row in rows:
+        print(row)
     
     
    
